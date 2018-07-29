1515b41921233ebc3ae7e253a01ec873028b16b3
Michael Ryan <michael_ryan_2000@yahoo.com>
2018-07-29 01:56:45 -0400

    Removed unnecessary loop from oobifier

--- commands/oobify.py
+++ commands/oobify.py
@@ -6,10 +6,8 @@ class Oobify(Command):
         super().__init__("oobify")
 
     def oob(self, string):
-        b_conflicts = re.findall('[aeiouy]b',string)
-        for b in b_conflicts:
-            string = string.replace(b, b[0])
-        new_string = re.sub('[aeiouy]','oob',string)
+        new_string = re.sub('[aeiouy]b','a', string)
+        new_string = re.sub('[aeiouy]','oob', new_string)
         new_string = re.sub('[AEIOUY]','Oob',new_string)
         return new_string
 
